c15b812e43839912b672c1e07ca37ba075a3fbd0
Paul Ralyea <047par@gmail.com>
2020-03-05 20:45:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -14,4 +14,12 @@ def link
 end
 
 def all_phrases
+  phrase = "It's-a me, Mario!"
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  say = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+  puts status
+  puts say
+end
+  
   
